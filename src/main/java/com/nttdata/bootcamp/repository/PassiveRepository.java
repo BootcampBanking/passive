86ebe586ff8d10504921b94e8528e9d490991bba
dmaldonf <dianacarolina8m@gmail.com>
2022-12-21 14:37:58 -0500

    update

--- src/main/java/com/nttdata/bootcamp/repository/PassiveRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nttdata.bootcamp.repository;
-
-import com.nttdata.bootcamp.entity.Passive;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface PassiveRepository extends ReactiveCrudRepository<Passive, String> {
-}
